47779d4b08cfcfef396812c5c77497d003e188ac
Hejkitty54 <kozue.kishimoto@.gmail.com>
2016-01-28 10:00:09 +0100

    first commit

---
+++ Myself/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Myself
+//
+//  Created by ITHS on 2016-01-27.
+//  Copyright © 2016 ITHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Myself/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Myself
+//
+//  Created by ITHS on 2016-01-27.
+//  Copyright © 2016 ITHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
